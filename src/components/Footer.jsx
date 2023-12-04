c9dba68f81caba488124d8be46cfc2ce98133c03
Akash Katuri <akashkaturi77@gmail.com>
2023-12-04 07:33:22 +0530

    Email Section - done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className={`footer border border-top[#33353F] border-l-transparent border-r-transparent`}>
+            <div className={`container p-14 mt-2 flex justify-between`}>
+                <span>AK</span>
+                <p className={`text-slate-600`}>All rights reserved.</p>
+            </div>
+        </footer>
+    )
+}
+export default Footer;
